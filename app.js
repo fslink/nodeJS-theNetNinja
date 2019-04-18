d42a935b64d05aaf9b667dc40bc1764694718fbd
FS_link <mowglysofiann@gmail.com>
2019-04-18 16:32:55 +0200

    Post request

--- app.js
+++ app.js
@@ -1,13 +1,50 @@
 var express = require('express');
-
+var http = require('http');
+var fs = require('fs');
 var app = express();
+var bodyParser = require('body-parser');
 
-app.get('/', function(req, res){
-	res.send('Welcome home!');
+var Obj = {
+	name: 'fuckers',
+	job: 'enculeur de mamans',
+	age: '40',
+	caracter: 'crevard'
+};
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+app.set('view engine', 'ejs');
+app.use('/assets', express.static('assets'));
+
+app.get(['/', '/home'], function(req, res){
+	res.render('index');
 });
 
 app.get('/contact', function(req, res){
-	res.send('Contact us');
+	console.log(req.query);
+	res.render('contact', {qs: req.query});
 });
 
-app.listen(3000);
+app.post('/contact', urlencodedParser, function(req, res){
+	console.log(req.body);
+	res.render('contact-sucess', {data: req.body});
+});
+
+app.get('/lorem', function(req, res){
+	var myReadStream = fs.createReadStream(__dirname + '/readMe.txt', 'utf8');
+	myReadStream.pipe(res);
+});
+
+app.get('/crevard', function(req, res){
+	console.log('the request url is: ' + req.url);
+	res.json(Obj);
+});
+
+app.get(['/profile/:name', '/profile'], function(req, res){
+	var data = {age: 29, job: 'ninja', hobbies: ['eating', 'swimming', 'fighting']}
+	res.render('profile', {person: req.params.name, data: data});
+});
+
+app.listen(3000, function(){
+	console.log('server on 3000 port');
+});
